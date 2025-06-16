3e84cdca0116d8375e3681856d2e4d3625847177
Masihullah37 <digitalmail372@gmail.com>
2025-06-16 12:29:37 +0200

    Final configuration for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { resolve } from 'path'
+
+// export default defineConfig({
+//   base: '/Portfolio_project/',
+//   plugins: [react()],
+//   build: {
+//     outDir: 'dist',
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html')
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
+// Check if we're in production
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
+  base: isProduction ? '/Portfolio_project/' : '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
