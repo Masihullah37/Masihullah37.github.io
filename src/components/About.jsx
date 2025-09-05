cda0d3e628f5fa4a3f771950d19af418bc6da094
Masihullah37 <digitalmail372@gmail.com>
2025-09-05 18:21:37 +0200

    Fix deploy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <h2 className="text-center gradient-text fw-bold mb-5 jetbrains-mono-bold">À propos de moi</h2>
         <div className="row justify-content-center">
           <div className="col-lg-8">
-            <p className="lead text-secondary mb-4 jetbrains-mono-regular">
+            <p className="lead text-white mb-4 jetbrains-mono-regular">
               Développeur Web et Web Mobile passionné par la création d'interfaces dynamiques, modernes et accessibles,
               je souhaite intégrer une entreprise innovante afin de mettre en œuvre mes compétences techniques et mon
               sens de l'organisation pour contribuer à des projets digitaux ambitieux.
