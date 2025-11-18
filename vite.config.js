e6ac2297992117eaea6cd2302a61d92226b9b9d5
Masihullah37 <digitalmail372@gmail.com>
2025-11-19 00:48:52 +0100

    Change base URL for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  
+  base: "/Masihullah37.github.io/",  
   css: {
     postcss: './postcss.config.js',
   }
